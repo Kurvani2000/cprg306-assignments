75bfb0bba134d6613893dafc30ddfe12e212900a
Kurvani2000 <kurvani2000@gmail.com>
2025-01-14 10:28:00 -0700

    week-2 demo

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() 
+{
+    return (
+        <main>
+            <h1>Gideon Simon</h1>
+            <Link href="https://github.com/Kurvani2000?tab=repositories">Github link</Link>
+        </main>
+    );
+  }
