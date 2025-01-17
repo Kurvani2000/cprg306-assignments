3343e7d3bafb06d0d54664f39a3b946e938c68a5
Kurvani2000 <kurvani2000@gmail.com>
2025-01-17 10:59:18 -0700

    week-2

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>My app week-2</h1>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p><Link href="/week-2">Go to Week-2</Link></p>
     </div>
   );
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -6,6 +6,7 @@ export default function Page()
     return (
       <main>
         <h1>Welcome to week-2</h1>
+        <StudentInfo/>
         <p><Link href="/">Go to Home</Link></p>
       </main>
     );
