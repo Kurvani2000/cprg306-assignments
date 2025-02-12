fd51a7ab7a5a62e7276a9b4e64c7fdf110a5c9da
Kurvani2000 <kurvani2000@gmail.com>
2025-02-12 10:32:26 -0700

    update

--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p><Link href="/week-2">Go to Week-2</Link></p>
-      <p><Link href="/week-3">Go to Week-3</Link></p>
-      <p><Link href="/week-4">Go to Week-4</Link></p>
-      <p><Link href="/week-5">Go to Week-5</Link></p>
+      <p><Link href="/week-2">Week-2</Link></p>
+      <p><Link href="/week-3">Week-3</Link></p>
+      <p><Link href="/week-4">Week-4</Link></p>
+      <p><Link href="/week-5">Week-5</Link></p>
     </div>
   );
 }
